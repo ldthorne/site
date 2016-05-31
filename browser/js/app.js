2375a58910a1d63edaa15c10f339d12f3f96e75e
Leon Thorne <ldthorne@brandeis.edu>
2016-05-31 00:08:28 -0400

    small things

--- browser/js/app.js
+++ browser/js/app.js
@@ -2,7 +2,6 @@
 window.app = angular.module('ThorneSite', ['fsaPreBuilt', 'ui.router', 'ui.bootstrap', 'ngAnimate', 'ngMaterial']);
 
 app.config(function ($urlRouterProvider, $locationProvider) {
-
   // This turns off hashbang urls (/#about) and changes it to something normal (/about)
   $locationProvider.html5Mode(true);
   // If we go to a URL that ui-router doesn't have registered, go to the "/" url.
