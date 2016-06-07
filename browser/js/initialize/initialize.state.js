6f8bc9c5ffbe228d724293c240244b3c1ffdd415
Leon Thorne <ldthorne@brandeis.edu>
2016-06-07 13:29:22 -0400

    put account edit stuff in directive

--- browser/js/initialize/initialize.state.js
+++ browser/js/initialize/initialize.state.js
@@ -3,12 +3,6 @@
 app.config(function ($stateProvider) {
   $stateProvider.state('initialize', {
     url: '/initialize',
-    templateUrl: 'js/initialize/initialize.template.html',
-    controller: 'InitCtrl',
-    resolve: {
-    	numUsers: (UserFactory) => {
-        return UserFactory.getUserCount();
-      }
-    }
+    templateUrl: 'js/initialize/initialize.template.html'
   });
 });
